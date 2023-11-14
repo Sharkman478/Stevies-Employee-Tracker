e25eb2ac513000949cffbcaeef83e9c67c780c4e
Sharkman478 <steviehelber@gmail.com>
2023-11-13 21:28:55 -0500

    commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'root',
+    database: 'employee_db',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the database');
+    start();
+});
+
+function start() {
+    inquirer
+      .prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+          'View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee role',
+          'Exit',
+        ],
+      })
+      .then((answer) => {
+        switch (answer.action) {
+          case 'View all departments':
+            viewAllDepartments();
+            break;
+          case 'View all roles':
+            viewAllRoles();
+            break;
+          case 'View all employees':
+            viewAllEmployees();
+            break;
+          case 'Add a department':
+            addDepartment();
+            break;
+          case 'Add a role':
+            addRole();
+            break;
+          case 'Add an employee':
+            addEmployee();
+            break;
+          case 'Update an employee role':
+            updateEmployeeRole();
+            break;
+          case 'Exit':
+            connection.end();
+            break;
+        }
+      })
+};
+
+function viewAllDepartments() {
+    connection.query('SELECT * FROM department', function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        start();
+    })
+};
